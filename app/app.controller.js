dab82c3bbe00c6e7261622a25ef320b73d96a249
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2017-06-05 17:45:22 +0800

    updated app controller to use this instead of

--- app/app.controller.js
+++ app/app.controller.js
@@ -2,16 +2,16 @@ angular
     .module('angular-starter')
     .controller('AppController', AppController)
 
-function AppController($scope) {
-    $scope.number = 1;
+function AppController() {
+    this.number = 1;
 
-    $scope.increment = function() {
-        $scope.number += 1;
+    this.increment = function() {
+        this.number += 1;
     }
 
-    $scope.decrement = function() {
-        if ($scope.number != 1) {
-            $scope.number -= 1;
+    this.decrement = function() {
+        if (this.number != 1) {
+            this.number -= 1;
         }
         else {
             alert("No zero value for version.");
